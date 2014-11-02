c9957ae369e8a9bb56f3e5f0d7cc7913a2277635
tetris11 <tetris11>
2014-11-02 22:40:35 +0000

    nowhia

--- diaspora_installer.sh
+++ diaspora_installer.sh
@@ -1,8 +1,45 @@
 #!/bin/bash
 
+# Arch:
 #sudo pacman -S python2-markdown tar aria2 jansson
 
+# Debian
+#sudo apt-get install\
+# libsdl1.2-dev libogg-dev libvorbis-dev\
+# libtheora-dev libopenal-dev libjansson-dev liblua5.1-0-dev libwxgtk2.8-dev
+
+
+progressfilt ()
+{
+    local flag=false c count cr=$'\r' nl=$'\n'
+    while IFS='' read -d '' -rn 1 c
+    do
+        if $flag; then
+            printf '%c' "$c"
+        else
+            if [[ $c != $cr && $c != $nl ]]; then
+                count=0
+            else
+                ((count++))
+                if ((count > 1)); then
+                    flag=true
+                fi
+            fi
+        fi
+    done
+}
+
+
 #runner="echo"
+
+### Config
+OS="ubuntu"
+#downloading="Y"
+#unpacking="Y"
+#patching="Y"
+building="Y"
+configuring="Y"
+
 
 # EDIT THESE:
 diaspora_main_url="http://diaspora.fs2downloads.com/Diaspora_R1_Linux.tar.lzma.torrent"
@@ -18,6 +55,10 @@ aria2_d="aria2c --console-log-level=error -V --seed-time=0 -O"
 #Get main file and 1.1 patch
 outfold=~/Desktop/temp_bob
 
+#launcher_patch_url="https://wxlauncher.googlecode.com/issues/attachment?aid=930010000&name=wxlauncher_homedir.patch&token=ABZ6GAdjC8In36ECIYOtPby8m0jYh99_3Q%3A1412610881778"
+#launcher_patch="wxLauncher.patch"
+wx_patchfile=$(readlink -f ./wxlauncher_homedir.patch)
+
 diaspora_main=$outfold/"Diaspora_R1_Linux.tar.lzma"
 diaspora_patch=$outfold/"Diaspora_R1_Patch_1.1.tar.lzma"
 
@@ -29,9 +70,14 @@ function untar_all(){
     tar -C $to --checkpoint=300 --checkpoint-action='ttyout=\r%u' --lzma -xf  $file
 }
 
-mkdir -p $outfold
 
-if ! [ : ]; then
+
+
+
+
+mkdir -p $outfold
+##########################################
+if [ -n "$downloading" ]; then
 
 cd $outfold
 
@@ -41,14 +87,26 @@ echo "========================="
 echo ""
 echo "Stage 1a: Downloading Main file (torrent)"
 $runner $aria2_d 1="$(basename $diaspora_main)" $diaspora_main_url
+[ "$?" != "0" ] && exit -1
+
 echo ""
 echo "Stage 1b: Downloading Patch"
-$runner $aria2_d 1="$(basename $diaspora_patch)" $diaspora_patch_url
+#$runner $aria2_d 1="$(basename $diaspora_patch)" $diaspora_patch_url
+$runner wget -c --progress=bar:force\
+ -O "$(basename $diaspora_patch)" $diaspora_patch_url\
+ 2>&1 | progressfilt
+
+[ "$?" != "0" ] && exit -1
 echo ""
 
 cd -
 
+fi
 
+#########################################
+if [ -n "$unpacking" ]; then
+
+echo ""
 echo "Stage 2a: Unpacking Main file"
 $runner mkdir -p $dest_dir
 $runner untar_all $dest_dir $diaspora_main 
@@ -58,41 +116,46 @@ $runner untar_all $dest_dir $diaspora_patch
 
 fi
 
-
 patch_file_tar=$(readlink -f $(find $dest_dir -name "*Patch*.tar"))
 diaspora_fold=$(readlink -f $(find $dest_dir -type d -name "Diaspora"))
 
-if ! [ : ]; then
+#################################################
+if [ -n "$patching" ]; then
 
-echo "Stage 2c: Applying Patch..."
-$runner tar -C $diaspora_fold --checkpoint=1000 --checkpoint-action=dot -xf $patch_file_tar
-echo ""
+	echo "Stage 2c: Applying Patch..."
+	$runner tar -C $diaspora_fold --checkpoint=1000 --checkpoint-action=dot -xf $patch_file_tar
+	echo ""
 
 fi
+
+##################################################
+if [ -n "$building" ]; then
+
 cd $diaspora_fold
 
-echo ""
-echo "Stage 3a: Building FS2 Open"
-cd fs2_open
-$runner eval "LDFLAGS=\"-l:liblua.so.5.1 $LDFLAGS\" CXXFLAGS=\"-I/usr/include/lua5.1 $CXXFLAGS\" ./autogen.sh"
-$runner make
-$runner mv code/fs2_open_* ../fs2_open_diaspora
+	echo ""
+	echo "Stage 3a: Building FS2 Open"
+	cd fs2_open
+	[ "$OS" = "arch" ] && $runner eval "LDFLAGS=\"-l:liblua.so.5.1 $LDFLAGS\" CXXFLAGS=\"-I/usr/include/lua5.1 $CXXFLAGS\" ./autogen.sh"
+	[ "$OS" = "ubuntu" ] && $runner eval "./autogen.sh"
+	$runner make
+	$runner mv code/fs2_open_* ../fs2_open_diaspora
 cd ..
 
 echo ""
 echo "Stage 3b: Building wxLauncher"
 
-launcher_patch_url=https://wxlauncher.googlecode.com/issues/attachment?aid=930010000&name=wxlauncher_homedir.patch&token=ABZ6GAdjC8In36ECIYOtPby8m0jYh99_3Q%3A1412610881778
-launcher_patch="wxLauncher.patch"
 apply_place="code/apis/"
 
 cd wxlauncher
-echo "Stage 3bi: Applying fix"
-$runner wget -O $launcher_patch $launcher_patch_url
-$runner mv $launcher_patch $apply_place
-cd $apply_place
-$runner patch < $launcher_patch
-cd ../../
+#if [ "$OS" = "arch" ];then
+	echo "Stage 3bi: Applying fix"
+	#$runner wget -O $launcher_patch $launcher_patch_url
+	$runner cp $wx_patchfile $apply_place
+	cd $apply_place
+	$runner patch < $(basename $wx_patchfile)
+	cd ../../
+#fi
 
 #back into wxLauncher
 cd build/
@@ -103,6 +166,13 @@ $runner cmake -DPYTHON_EXECUTABLE:FILEPATH=/usr/bin/python2\
  -DwxWidgets_CONFIG_EXECUTABLE=/usr/bin/wx-config-2.8 ../
 $runner make
 cd ../../
+
+
+fi
+
+
+##################################################
+if [ -n "$configuring" ];then
 
 #back into Diaspora fold
 echo ""
@@ -120,3 +190,5 @@ $runner ./wxlauncher/build/wxlauncher --select-profile --profile=Diaspora
 echo ""
 echo ""
 echo "Finished: Run \"$(readlink -f ./wxlauncher/build/wxlauncher)\""
+
+fi
